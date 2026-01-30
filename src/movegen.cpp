a05b65aeae9deee22b3435c7860e5125f3e2eed7
Apple <apple@Apples-MacBook.local>
2026-01-31 03:15:56 +0530

    fixed pawn move logic and compiled

--- src/movegen.cpp
+++ src/movegen.cpp
@@ -106,6 +106,64 @@ void generateallmoves(const Board &board, movelist &movelist){
                     }
                 }
             }
+        int knightrow[8]={2,2,-2,-2,1,1,-1,-1};
+        int knightcol[8]={-1,1,-1,1,-2,2,-2,-2};
+        if(p.type==KNIGHT){
+            for(int i=0;i<8;i++){
+                int nextrow=row+knightrow[i];
+                int nextcol=col+knightcol[i];
+                if(nextrow>0&&nextrow<8&&nextcol>0&&nextcol<8)
+                continue;
+                piece target=board.squares[nextrow][nextcol];
+                if(p.type==NO_PIECE||p.color!=target.color){
+                    move m;
+                    m.fromcol=col;
+                    m.fromrow=row;
+                    m.tocol=nextcol;
+                    m.torow=nextrow;
+                    movelist.moves[movelist.cnt++]=m;
+                }
+            }
         }
+            if(p.type==BISHOP){
+                int birow[4]={1,-1,1,-1};
+                int bicol[4]={-1,-1,1,1};
+                for(int i=0;i<4;i++){
+                    int r=row+birow[i];
+                    int c=col+bicol[i];
+                    while(r>0&&r<8&&c>0&&c<8){
+                        piece target=board.squares[r][c];
+                        if(target.type==NO_PIECE){
+                            move m;
+                            m.fromcol=col;
+                            m.fromrow=row;
+                            m.tocol=c;
+                            m.torow=r;
+                            movelist.moves[movelist.cnt++]=m;
+
+                        }
+                    
+                    else{
+                        if(target.color!=p.color){
+                               move m;
+                    m.fromrow = row;
+                    m.fromcol = col;
+                    m.torow = r;
+                    m.tocol = c;
+                    movelist.moves[movelist.cnt++] = m;
+                }
+                break; 
+                        }
+                        r+=birow[i];
+                        c+=bicol[i];
+                    }
+                
+                }
+            }
+            
+        }
+        
+
     }
+        
 }
